11ed469ec66d3501ed3d7a3f3561fa58854e8912
ok8023 <130885038+ok8023@users.noreply.github.com>
2023-04-20 12:33:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo "Noisework_To_Replit 一键搭建脚本"
 echo "脚本作者：ok8023"
 echo "开源地址：https://github.com/ok8023/Noisework_To_Replit/"
-git clone https://github.com/ok8023/Noisework_To_Replit && mv -b Noisework_To_Replit/* ./ && mv -b Noisework_To_Replit/.[^.]* ./ && rm -rf *~ && rm -rf Noisework_To_Replit && rm -rf index.php
+git clone https://github.com/ok8023/Noisework_To_Replit && mv -b Noisework_To_Replit/* ./ && mv -b Noisework_To_Replit/.[^.]* ./ && rm -rf *~ && rm -rf Noisework_To_Replit && rm -rf index.php && rm -rf install.sh
 echo "恭喜项目搭建完成"
 echo "点击Run立即运行项目"
 echo "源码官仓：https://github.com/rcy1314/noisework"
